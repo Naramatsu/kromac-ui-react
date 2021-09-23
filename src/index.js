1e9e3244c60df91ca1b241d6799c8f0e0bd443b8
Naramatsu <joncyy_19@hotmail.com>
2021-09-23 17:25:45 -0500

    added necessary tools to deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 
 const Main = lazy(() => import("./Main"));
-console.log("OS pc => ", window.navigator.appVersion.toLowerCase());
+// console.log("OS pc => ", window.navigator.appVersion.toLowerCase());
 
 ReactDOM.render(
   <Suspense fallback={<div>Loading Kromac UI...</div>}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,49 @@
-var path = require("path");
+const path = require('path');
+
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/components/lib/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve('lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
-          }
-        }
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'css-loader',
+          'sass-loader',
+          // 'style-loader',
+        ]
       }
-    ]
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
   },
   externals: {
-    react: "commonjs react"
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   }
 };
