58e46e2894a1a692d15de054d1089681694efd82
Narmatsu <joncyy_19@hotmail.com>
2022-10-25 17:07:23 -0500

    release page

--- src/components/ReleaseInfo/ReleaseInfo.js
+++ src/components/ReleaseInfo/ReleaseInfo.js
@@ -1,12 +1,12 @@
-import React, { lazy, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { changeDocumentTitle } from "../../utils/utils";
+import React, { lazy, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { changeDocumentTitle } from '../../utils/utils';
 
-const TextAnimation = lazy(() => import("../lib/TextAnimation"));
+const TextAnimation = lazy(() => import('../lib/TextAnimation'));
 
 const ReleaseInfo = () => {
   useEffect(() => {
-    document.title = changeDocumentTitle({ component: "Releases", state: "" });
+    document.title = changeDocumentTitle({ component: 'Releases', state: '' });
   });
   return (
     <div className="kromac-scroll landingpage release">
@@ -20,98 +20,50 @@ const ReleaseInfo = () => {
             fontColor="#fff"
           />
           <p>
-            In this version <b>1.2.4</b> we released the new
+            In this version <b>1.2.5</b> we released news components
             <b>
-              <i> 'video' </i>
+              <i> 'Grid' </i>
             </b>
-            feature in several components
+            and
+            <b>
+              <i> 'GridItem'. </i>
+            </b>
           </p>
           <p>
-            The video property was added and the image property is no longer
-            required, we can decide which one is better.
+            Now you can use
+            <b>
+              <i> 'Grid' </i>
+            </b>
+            component to custom your own display columns from 1 to 24 and
+            <b>
+              <i> 'GridItem' </i>
+            </b>
+            component to define how many columns you want and even how may rows
+            per item.
           </p>
           <br />
           <p>
-            Now you can send a video property in the follow compontnes in this
-            way:
+            Visit &nbsp;
+            <Link
+              to={{
+                pathname: 'grid',
+              }}
+              name="grid"
+            >
+              Grid
+            </Link>
+            &nbsp; and &nbsp;
+            <Link
+              to={{
+                pathname: 'gridItem',
+              }}
+              name="gridItem"
+            >
+              GridItem
+            </Link>
+            &nbsp; to know how to implement.
           </p>
           <br />
-          <ul>
-            <li className="li-container">
-              <div className="div-components">
-                <h5>Avatar</h5>
-                <h5>Card Classic</h5>
-                <h5>Card Horizontal</h5>
-                <h5>Card Image</h5>
-                <h5>Card Polygon</h5>
-                <h5>Card Reveal</h5>
-                <h5>Card Team</h5>
-              </div>
-              <div>
-                <h5>implementation</h5>
-                <pre>
-                  {`video={{
-  url: video.mp4, //required
-  controls: false, //default
-  autoPlay: true, //default
-  muted: true, //default
-  loop: true //default
-}}`}
-                </pre>
-              </div>
-            </li>
-            <li className="li-container">
-              <div className="div-components">
-                <h5>Gallery responsive</h5>
-              </div>
-              <div>
-                <h5>implementation</h5>
-                <pre>
-                  {`content={[
-  {
-    video: video.mp4,
-    row: 2 // optional || [2-4]
-    col: 2 // optional || [2-4]
-  },
-  {
-    img: video.mp4,
-    row: 2 // optional || [2-4]
-    col: 2 // optional || [2-4]
-  }
-]}`}
-                </pre>
-              </div>
-            </li>
-          </ul>
-
-          <div className="release-notes">
-            <h4>Notes: </h4>
-            <ul>
-              <li>
-                <p>
-                  fixed wrong layout in card team summary.
-                </p>
-              </li>
-              <li>
-                <p>
-                  One of the advantages of this new implementation is that you
-                  can pass the url of videos hosted in a folder (in the cloud or
-                  locally) or youtube links (taken from the desktop version or
-                  the mobile version).
-                </p>
-              </li>
-              <li>
-                <p>
-                  <b>
-                    <i>
-                      Youtube links generated with iframe are not yet supported
-                      in this version, but we will work to support it.
-                    </i>
-                  </b>
-                </p>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     </div>
