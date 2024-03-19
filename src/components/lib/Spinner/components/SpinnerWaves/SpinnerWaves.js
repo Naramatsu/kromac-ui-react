040f25226ddf11f656803eac2f432491ea404b8f
Narmatsu <joncyy_19@hotmail.com>
2024-03-19 11:24:50 -0500

    sorting scss properties

--- src/components/lib/Spinner/components/SpinnerWaves/SpinnerWaves.js
+++ src/components/lib/Spinner/components/SpinnerWaves/SpinnerWaves.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import classNames from 'classnames';
-import { styleCentered } from '../../../../../utils/utils';
+import React from "react";
+import classNames from "classnames";
+import { styleCentered } from "../../../../../utils/utils";
 
 const spanGenerator = (loops) => {
   const spans = [];
   for (let i = 1; i <= loops; i++) {
-    var style = { '--i': i };
+    var style = { "--i": i };
     spans.push(<span key={i} style={style} />);
   }
   return spans;
 };
 
 const SpinnerWaves = (props) => {
-  const { size = 'sm', isCentered = false, shadows = true, ...rest } = props;
+  const { size = "sm", isCentered = false, shadows = true, ...rest } = props;
   const { key, id } = rest;
   const newRest = {
     key,
     id,
   };
-  const isShadows = shadows ? 'shadows' : '';
-  const kromacSpinner = classNames('kromac-spinner', 'spinner-waves', {
+  const isShadows = shadows ? "shadows" : "";
+  const kromacSpinner = classNames("kromac-spinner", "spinner-waves", {
     [size]: !!size,
     [isShadows]: !!isShadows,
     [rest.className]: !!rest.className,
