58be5c5896c544765b81b0b44c0e9dccffbdab2a
Narmatsu <joncyy_19@hotmail.com>
2024-03-22 14:41:28 -0500

    new releases changes were referenced

--- src/components/ReleaseInfo/ReleaseInfo.js
+++ src/components/ReleaseInfo/ReleaseInfo.js
@@ -8,8 +8,9 @@ const ReleaseInfo = () => {
   useEffect(() => {
     document.title = changeDocumentTitle({ component: "Releases", state: "" });
   });
+
   return (
-    <div className="kromac-scroll landingpage release">
+    <div className="kromac-scroll landingpage release-notes">
       <div className="kromac-landingpage-container">
         <Link to="/"> {"<"} Go back to home</Link>
         <br />
@@ -20,32 +21,48 @@ const ReleaseInfo = () => {
             fontColor="#fff"
           />
           <p>
-            In this version <b>1.3.0</b> we released a new component
+            In this version <b>1.3.0</b> we released a new component called
             <b>
               <i> 'Chip' </i>
             </b>
-            .
+            and made other necessary adjustments:
           </p>
           <br />
-          <p>
-            We adjust the
-            <b>
-              <i> "box-shadow" </i>
-            </b>
-            value to another softer in all components.
-          </p>
-          <p>
-            And also we renamed the &nbsp;
-            <b>
-              CardTeam
-              <i> "redes" </i>
-            </b>
-            property by
-            <b>
-              <i> "media" </i>
-            </b>
-            .
-          </p>
+          <ul>
+            <li>
+              <p>
+                We adjust the
+                <b>
+                  <i> "box-shadow" </i>
+                </b>
+                value to another softer in all components.
+              </p>
+            </li>
+            <li>
+              <p>
+                A new Toast prop
+                <b>
+                  <i> "alwaysVisible" </i>
+                </b>
+                was added.
+              </p>
+            </li>
+            <li>
+              <p>
+                And also we renamed the &nbsp;
+                <b>
+                  CardTeam
+                  <i> "redes" </i>
+                </b>
+                property by
+                <b>
+                  <i> "media" </i>
+                </b>
+                .
+              </p>
+            </li>
+          </ul>
+
           <br />
           <p>
             Visit &nbsp;
@@ -56,6 +73,18 @@ const ReleaseInfo = () => {
               name="chip"
             >
               Chip
+            </Link>
+            &nbsp; to know how to implement.
+          </p>
+          <p>
+            Visit &nbsp;
+            <Link
+              to={{
+                pathname: "toast",
+              }}
+              name="toast"
+            >
+              Toast
             </Link>
             &nbsp; to know how to implement.
           </p>
